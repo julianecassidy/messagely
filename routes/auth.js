aa389cb3c61100248d0147d564adfcd9ddf6209b
Vaughn Seekamp <vseekamp@gmail.com>
2023-04-11 11:22:01 -0700

    verified auth routes

--- routes/auth.js
+++ routes/auth.js
@@ -5,7 +5,7 @@ const router = new Router();
 const { authenticate, updateLoginTimestamp, register } = require("../models/user");
 const { BadRequestError, UnauthorizedError } = require('../expressError');
 const { SECRET_KEY } = require('../config.js');
-const jwt = require('jsonwebtoken')
+const jwt = require('jsonwebtoken');
 
 
 /** POST /login: {username, password} => {token} */
@@ -24,7 +24,7 @@ router.post('/login', async function (req, res, next) {
   } else {
     throw new UnauthorizedError('Invalid username/password');
   }
-})
+});
 
 /** POST /register: registers, logs in, and returns token.
  *
@@ -37,21 +37,26 @@ router.post('/register', async function (req, res, next) {
   const user = req.body;
   // console.log(user);
 
-  for (let keys in user) {
-    if (user[keys] === undefined) {
+  let values = Object.values(user);
+  if (values.length < 5) {
+    throw new BadRequestError('All fields must be filled out.');
+  }
+  for (let val of values) {
+    if (val === undefined || val === '') {
       throw new BadRequestError('All fields must be filled out.');
     }
-  };
+  }
+
   let registerUser;
   try {
     registerUser = await register(user); //Maybe an issue
-  } catch(error) {
+  } catch (error) {
     throw new BadRequestError('Username must be unique');
   }
 
   await updateLoginTimestamp(registerUser.username);
-  const token = jwt.sign({username: registerUser.username}, SECRET_KEY);
+  const token = jwt.sign({ username: registerUser.username }, SECRET_KEY);
   return res.json({ token });
-})
+});
 
 module.exports = router;
