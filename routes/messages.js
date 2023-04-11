7eac294c16fbacc0fd23516563cc48a16a93a006
Vaughn Seekamp <vseekamp@gmail.com>
2023-04-11 12:28:17 -0700

    messages routes

--- routes/messages.js
+++ routes/messages.js
@@ -1,5 +1,9 @@
 "use strict";
 
+const { UnauthorizedError } = require("../expressError");
+const { ensureLoggedIn } = require("../middleware/auth");
+const { markRead, get, create } = require("../models/message");
+
 const Router = require("express").Router;
 const router = new Router();
 
@@ -15,7 +19,16 @@ const router = new Router();
  * Makes sure that the currently-logged-in users is either the to or from user.
  *
  **/
+router.get('/:id', ensureLoggedIn, async function (req, res, next){
 
+  let messages = await get(req.params.id);
+  if (res.locals.user.username === messages.to_user.username ||
+    res.locals.user.username === messages.from_user.username) {
+      return res.json({ messages });
+    } else {
+      throw new UnauthorizedError();
+    }
+});
 
 /** POST / - post message.
  *
@@ -24,6 +37,17 @@ const router = new Router();
  *
  **/
 
+router.post('/', ensureLoggedIn, async function (req, res, next) {
+  let currUser = res.locals.user.username;
+  let toUser = req.body.to_username;
+  let messageBody = req.body.body;
+  let newMessage = { from_username: currUser,
+    to_username: toUser,
+    body: messageBody
+  }
+  let createMessage = await create(newMessage);
+  return res.json(createMessage);
+});
 
 /** POST/:id/read - mark message as read:
  *
@@ -32,6 +56,17 @@ const router = new Router();
  * Makes sure that the only the intended recipient can mark as read.
  *
  **/
+
+router.post('/:id/read', ensureLoggedIn, async function (req, res, next){
+
+  let messageId = await get(req.params.id);
+  if (res.locals.user.username === messages.to_user.username) {
+      let message = markRead(messageId.id)
+      return res.json({ message });
+    } else {
+      throw new UnauthorizedError();
+    }
+});
 
 
 module.exports = router;
